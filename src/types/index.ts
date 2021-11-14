32ce1be8b7ea67493371d53e724eed150d058705
zhongpeng <859247268@qq.com>
2021-11-14 23:56:46 +0800

    feat: 返回值处理逻辑

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export type Method =
-  | 'get'
+   'get'
   | 'GET'
   | 'delete'
   | 'DELETE'
@@ -20,4 +20,18 @@ export interface AxiosRequestConfig {
   data?: any
   params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  requset: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {
+    
 }
